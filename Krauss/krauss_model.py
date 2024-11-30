efbb4c078ae98a78dfdcd8cee5b4dfc73fc8d4d6
Oskar Matysik <oskar.orion18@gmail.com>
2024-11-30 14:37:42 +0100

    poprawka w modelu kraussa, testy

--- Krauss/krauss_model.py
+++ Krauss/krauss_model.py
@@ -6,9 +6,25 @@ import matplotlib.colors as colors
 import os
 from PIL import Image
 
+
 class KraussSimulation:
 
-    def __init__(self, num, acc_coeff=1, br_coeff=1, eps=1, v_max=40, road_length=1000, num_cars=30, time=20, reaction_time=1):
+    def __init__(
+        self,
+        num,
+        acc_coeff=0.2,
+        br_coeff=0.6,
+        eps=1,
+        v_max=5,
+        road_length=200,
+        num_cars=100,
+        time=100,
+        reaction_time=1,
+        create_gif=False,
+        jam_speed_coeff=0.2,
+        jam_gap_coeff=0.2,
+        jam_cars_involved_coeff=0.1
+    ):
         self.num = num
         self.acc_coeff = acc_coeff
         self.br_coeff = br_coeff
@@ -21,7 +37,11 @@ class KraussSimulation:
         self.x, self.v = self._generate_cars()
         self.history_x = np.empty((self.time, self.num_cars))
         self.history_v = np.empty((self.time, self.num_cars))
-    
+        self.create_gif = create_gif
+        self.jam_speed_coeff = jam_speed_coeff
+        self.jam_gap_coeff = jam_gap_coeff
+        self.jam_cars_involved_coeff = jam_cars_involved_coeff
+
     def _generate_cars(self):
         x = np.sort(np.random.uniform(0, self.road_length, self.num_cars))
         v = np.random.uniform(0, self.v_max, self.num_cars)
@@ -33,31 +53,32 @@ class KraussSimulation:
         path = f'{os.getcwd()}/Krauss/simulations/{str(self.num).rjust(2, "0")}'
         os.mkdir(path)
         self._krauss_simulation()
-        avg_speed, max_speed, min_speed, avg_position, std_position, avg_gaps, max_gaps, min_gaps = self._get_stats()
-        self._plot_space()
-        self._plot_to_gif(path)
-        self._plot_speed_stats(path, avg_speed, max_speed, min_speed)
-        self._plot_position_stats(path, avg_position, std_position)
-        self._plot_gaps_stats(path, avg_gaps, max_gaps, min_gaps)
+        if self.create_gif:
+            self._plot_space()
+            self._plot_to_gif(path)
+        self._plot_speed_stats(path)
+        self._plot_traffic_flow_stats(path)
+        self._plot_jam_stats(path)
         self._plot_history(path)
 
     def _krauss_simulation(self):
 
         for i in range(self.time):
             self.x, self.v = self._krauss_step(i)
-        #return self.history_x, self.history_v
-    
+
     def _krauss_step(self, step):
         n = len(self.v)
         v_new = np.empty(n)
         x_new = np.empty(n)
-        gaps = np.array([self.x[(i+1) % n] - self.x[i] for i in range(n)])
+        gaps = np.array([self.x[(i + 1) % n] - self.x[i] for i in range(n)])
         gaps[-1] += self.road_length
         for i in range(n):
-            full_stop_time = (self.v[i] + self.v[(i+1) % n])/2*self.br_coeff
+            full_stop_time = (self.v[i] + self.v[(i + 1) % n]) / (2 * self.br_coeff)
             gap = gaps[i]
-            gap_des = self.reaction_time * self.v[(i+1) % n]
-            v_safe = self.v[(i+1) % n] + (gap - gap_des)/(self.reaction_time + full_stop_time)
+            gap_des = self.reaction_time * self.v[(i + 1) % n]
+            v_safe = self.v[(i + 1) % n] + (gap - gap_des) / (
+                self.reaction_time + full_stop_time
+            )
             v_des = min([self.v_max, self.v[i] + self.acc_coeff, v_safe])
             v_new[i] = max(0, v_des - np.random.uniform(0, self.eps))
 
@@ -67,91 +88,118 @@ class KraussSimulation:
         for i in range(n):
             x_new[i] = self.x[i] + v_new[i]
         return np.sort(x_new), v_new
-    
-    def _get_stats(self):
-        n = np.size(self.history_x, 1)
-        avg_speed = [np.mean(self.history_v[i]) for i in range(self.time)]
-        max_speed = [np.max(self.history_v[i]) for i in range(self.time)]
-        min_speed = [np.min(self.history_v[i]) for i in range(self.time)]
-        avg_position = [np.mean(self.history_x[i]) for i in range(self.time)]
-        std_position = [np.std(self.history_x[i]) for i in range(self.time)]
 
-        gaps = [[(self.history_x[j][(i+1) % n] - self.history_x[j][i]) % self.road_length for i in range(n)] for j in range(self.time)]
-        gaps_avg = [np.mean(gaps[i]) for i in range(self.time)]
-        gaps_max = [np.max(gaps[i]) for i in range(self.time)]
-        gaps_min = [np.min(gaps[i]) for i in range(self.time)]
-    
-        return avg_speed, max_speed, min_speed, avg_position, std_position, gaps_avg, gaps_max, gaps_min
-    
     def _plot_space(self):
         n = len(self.history_x[0])
         for i in range(self.time):
-            plt.scatter(self.history_x[i], [0]*n, c=self.history_v[i], cmap='viridis', vmax=40, vmin=0)
+            plt.scatter(
+                self.history_x[i],
+                [0] * n,
+                c=self.history_v[i],
+                cmap="viridis",
+                vmax=self.v_max,
+                vmin=0,
+                s=3,
+            )
             plt.colorbar()
             plt.xlim((0, self.road_length))
-            plt.title(f't = {i} $\Delta t = 1$')
+            plt.title(f"t = {i} $\Delta t = 1$")
             plt.savefig(f'Krauss/graphs/{str(i+1).rjust(4, "0")}.png')
             plt.clf()
-    
+
     def _plot_to_gif(self, path):
-        source = f'{os.getcwd()}/Krauss/graphs'
-        images = [Image.open(source + '/' + file) for file in os.listdir(source)]
-        images[0].save(f'{path}/simulation_gif.gif', 
-                    save_all=True,
-                    append_images=images[1:],
-                    duration=100,
-                    loop=0
-                    )
-        for file in os.listdir(f'{os.getcwd()}/Krauss/graphs'):
-            os.remove(os.path.join(f'{os.getcwd()}/Krauss/graphs', file))
-    
-    def _plot_speed_stats(self, path, avg_speed, max_speed, min_speed):
+        source = f"{os.getcwd()}/Krauss/graphs"
+        images = [Image.open(source + "/" + file) for file in os.listdir(source)]
+        images[0].save(
+            f"{path}/simulation_gif.gif",
+            save_all=True,
+            append_images=images[1:],
+            duration=100,
+            loop=0,
+        )
+        for file in os.listdir(f"{os.getcwd()}/Krauss/graphs"):
+            os.remove(os.path.join(f"{os.getcwd()}/Krauss/graphs", file))
+
+    def _plot_speed_stats(self, path):
         t = np.arange(self.time)
-        plt.plot(t, avg_speed, color='red', label='$v_{śr}$')
-        plt.plot(t, max_speed, color='green', label='$v_{max}$')
-        plt.plot(t, min_speed, color='blue', label='$v_{min}$')
+        avg_speed = [np.mean(self.history_v[i]) for i in range(self.time)]
+        max_speed = [np.max(self.history_v[i]) for i in range(self.time)]
+        min_speed = [np.min(self.history_v[i]) for i in range(self.time)]
+        plt.figure(dpi=1200)
+        plt.plot(t, avg_speed, color="red", label="$v_{śr}$")
+        plt.plot(t, max_speed, color="green", label="$v_{max}$")
+        plt.plot(t, min_speed, color="blue", label="$v_{min}$")
         plt.legend()
-        plt.xlabel('Czas')
-        plt.ylabel('Prędkość')
-        plt.savefig(f'{path}/speed_statistics.png')
+        plt.xlabel("Czas")
+        plt.ylabel("Prędkość")
+        plt.savefig(f"{path}/speed_statistics.png")
         plt.clf()
 
-    def _plot_position_stats(self, path, avg_position, std_position):
+    def _plot_traffic_flow_stats(self, path):
         t = np.arange(self.time)
-        plt.plot(t, avg_position, color='red', label='$x_{śr}$')
-        plt.plot(t, std_position, color='blue', label='$\sigma_x$')
+        avg_speed = np.array([np.mean(self.history_v[i]) for i in range(self.time)])
+        traffic_flow = avg_speed * self.num_cars / self.road_length
+        plt.figure(dpi=1200)
+        plt.plot(t, traffic_flow, color="red", label="Przepływ")
         plt.legend()
-        plt.xlabel('Czas')
-        plt.ylabel('Wartość x')
-        plt.savefig(f'{path}/position_statistics.png')
+        plt.xlabel("Czas")
+        plt.ylabel("Przepływ")
+        plt.savefig(f"{path}/position_statistics.png")
         plt.clf()
 
-    def _plot_gaps_stats(self, path, avg_gaps, max_gaps, min_gaps):
+    def _plot_jam_stats(self, path):
         t = np.arange(self.time)
-        plt.plot(t, avg_gaps, color='red', label='$d_{śr}$')
-        plt.plot(t, max_gaps, color='green', label='$d_{max}$')
-        plt.plot(t, min_gaps, color='blue', label='$d_{min}$')
+        g_hom = self.num_cars / self.road_length
+        v_hom = g_hom
+        gaps = np.array([
+            [
+                (self.history_x[j][(i + 1) % self.num_cars] - self.history_x[j][i])
+                % self.road_length
+                for i in range(self.num_cars)
+            ]
+            for j in range(self.time)
+        ])
+        
+        traffic_jam_state = self.jam_cars_involved_coeff * self.num_cars
+        jam_cars = np.empty(self.time)
+        for i in range(self.time):
+            jam_velocity_condition = self.history_v[i] < self.jam_speed_coeff * v_hom
+            jam_gap_condition = gaps[i] < self.jam_gap_coeff * g_hom
+            # print(gaps)
+            # print(jam_gap_condition)
+            jam_cars[i] = np.sum(jam_gap_condition & jam_velocity_condition)
+        
+        plt.figure(dpi=1200)
+        plt.plot(t, jam_cars, color="blue", label="Liczba pojazdów")
+        plt.axhline(traffic_jam_state, 0, self.time, color="red", label="Stan korku")
         plt.legend()
-        plt.xlabel('Czas')
-        plt.ylabel('Odległości między samochodami')
-        plt.savefig(f'{path}/gaps_statistics.png')
+        plt.xlabel("Czas")
+        plt.ylabel("Liczba samochodów stojących w korku")
+        plt.savefig(f"{path}/gaps_statistics.png")
         plt.clf()
 
     def _plot_history(self, path):
         n = len(self.history_x[0])
+        plt.figure(dpi=1200)
         for i in range(self.time):
-            plt.scatter(self.history_x[i], [i]*n, c=self.history_v[i], cmap='viridis', vmax=40, vmin=0)
+            plt.scatter(
+                self.history_x[i],
+                [i] * n,
+                c=self.history_v[i],
+                cmap="viridis",
+                vmax=self.v_max,
+                vmin=0,
+                s=1,
+            )
         plt.colorbar()
         plt.xlim((0, self.road_length))
         plt.gca().invert_yaxis()
-        plt.savefig(f'{path}/simulation.png')
+        plt.axis('scaled')
+        plt.savefig(f"{path}/simulation.png")
         plt.clf()
 
     ###########################################################
 
-if __name__ == '__main__':
-    #full_krauss_simulation(0, time=200)
-    #full_krauss_simulation(1, time=1000, num_cars=100, road_length=5000)
-    #full_krauss_simulation(2, time=2000, num_cars=300, road_length=5000)
-    KraussSimulation(0).simulation()
 
+if __name__ == "__main__":
+    KraussSimulation(19, eps=1, time=500).simulation()
--- RL/rl_model.py
+++ RL/rl_model.py
@@ -6,9 +6,26 @@ import matplotlib.colors as colors
 import os
 from PIL import Image
 
+
 class QLearningSimulation:
-    def __init__(self, num, acc_coeff=1, br_coeff=1, eps=1, v_max=40, road_length=1000, num_cars=30, 
-                 time=20, reaction_time=1, v_disc=41, v_prev_disc=21, gap_disc=21, alpha=0.5, gamma=0.75):
+    def __init__(
+        self,
+        num,
+        acc_coeff=0.2,
+        br_coeff=0.6,
+        eps=1,
+        v_max=5,
+        road_length=200,
+        num_cars=100,
+        time=100,
+        reaction_time=1,
+        v_disc=41,
+        v_prev_disc=21,
+        gap_disc=21,
+        alpha=0.1,
+        gamma=0.99,
+        create_gif=False
+    ):
         self.num = num
         self.acc_coeff = acc_coeff
         self.br_coeff = br_coeff
@@ -28,7 +45,8 @@ class QLearningSimulation:
         self.gap_disc = np.arange(0, v_max, gap_disc)
         self.q_table_steady = np.zeros((v_disc, v_prev_disc, gap_disc))
         self.q_table_accelerate = np.zeros((v_disc, v_prev_disc, gap_disc))
-    
+        self.create_gif = create_gif
+
     def _generate_cars(self):
         x = np.sort(np.random.uniform(0, self.road_length, self.num_cars))
         v = np.random.uniform(0, self.v_max, self.num_cars)
@@ -39,79 +57,125 @@ class QLearningSimulation:
         path = f'{os.getcwd()}/RL/simulations/{str(self.num).rjust(2, "0")}'
         os.mkdir(path)
         self._qlearning_simulation()
-        avg_speed, max_speed, min_speed, avg_position, std_position, avg_gaps, max_gaps, min_gaps = self._get_stats()
-        self._plot_space()
-        self._plot_to_gif(path)
+        (
+            avg_speed,
+            max_speed,
+            min_speed,
+            avg_position,
+            std_position,
+            avg_gaps,
+            max_gaps,
+            min_gaps,
+        ) = self._get_stats()
+        if self.create_gif:
+            self._plot_space()
+            self._plot_to_gif(path)
         self._plot_speed_stats(path, avg_speed, max_speed, min_speed)
         self._plot_position_stats(path, avg_position, std_position)
         self._plot_gaps_stats(path, avg_gaps, max_gaps, min_gaps)
         self._plot_history(path)
+        print(np.sum(self.q_table_accelerate > self.q_table_steady))
 
     def _qlearning_simulation(self):
         for i in range(self.time):
             self.x, self.v = self._qlearning_step(i)
-        #return self.history_x, self.history_v
-    
+        # return self.history_x, self.history_v
+
     def _qlearning_step(self, step):
         n = len(self.v)
         v_new = np.empty(n)
         x_new = np.empty(n)
-        gaps = np.array([self.x[(i+1) % n] - self.x[i] for i in range(n)])
+        gaps = np.array([self.x[(i + 1) % n] - self.x[i] for i in range(n)])
         gaps[-1] += self.road_length
         for i in range(n):
-            
+
             # wyznaczenie dyskretnych współrzędnych obecnego stanu w Q-table
-            
-            state = [np.searchsorted(self.v_disc, self.v[i]),
-                              np.searchsorted(self.v_prev_disc, self.v[(i-1) % n]),
-                              np.searchsorted(self.gap_disc, gaps[i])] 
-            
+
+            state = [
+                np.searchsorted(self.v_disc, self.v[i]),
+                np.searchsorted(self.v_prev_disc, self.v[(i - 1) % n]),
+                np.searchsorted(self.gap_disc, gaps[i]),
+            ]
+
             # wyznaczenie prędkości przy założeniu, że przyspieszymy/będziemy zmuszeni żeby zwolnić
-            
-            full_stop_time = (self.v[i] + self.v[(i+1) % n])/2*self.br_coeff
-            gap_des = self.reaction_time * self.v[(i+1) % n]
-            v_safe = self.v[(i+1) % n] + (gaps[i] - gap_des)/(self.reaction_time + full_stop_time)
+
+            full_stop_time = (self.v[i] + self.v[(i + 1) % n]) / 2 * self.br_coeff
+            gap_des = self.reaction_time * self.v[(i + 1) % n]
+            v_safe = self.v[(i + 1) % n] + (gaps[i] - gap_des) / (
+                self.reaction_time + full_stop_time
+            )
             v_des = min([self.v_max, self.v[i] + self.acc_coeff, v_safe])
-            
-            # podjęcie decyzji 
-            
+
+            # podjęcie decyzji
+
             if v_des > self.v[i]:
-                
-                if self.q_table_accelerate[state[0], state[1], state[2]] >= self.q_table_steady[state[0], state[1], state[2]]:
-                    action = 'accelerate'
-                    state_new = [np.searchsorted(self.v_disc, v_des), state[1], state[2]]
+
+                if (
+                    self.q_table_accelerate[state[0], state[1], state[2]]
+                    > self.q_table_steady[state[0], state[1], state[2]]
+                ):
+                    action = "accelerate"
+                    state_new = [
+                        np.searchsorted(self.v_disc, v_des),
+                        state[1],
+                        state[2],
+                    ]
                     v_new[i] = v_des
+                elif (
+                    self.q_table_accelerate[state[0], state[1], state[2]]
+                    == self.q_table_steady[state[0], state[1], state[2]]
+                ):
+                    if np.random.random() > 0.5:
+                        action = "accelerate"
+                        state_new = [
+                            np.searchsorted(self.v_disc, v_des),
+                            state[1],
+                            state[2],
+                        ]
+                        v_new[i] = v_des
+                    else:
+                        action = "steady"
+                        state_new = [
+                            np.searchsorted(self.v_disc, self.v[i]),
+                            state[1],
+                            state[2],
+                        ]
+                        v_new[i] = self.v[i]
                 else:
-                    action = 'steady'
-                    state_new = [np.searchsorted(self.v_disc, self.v[i]), state[1], state[2]]
+                    action = "steady"
+                    state_new = [
+                        np.searchsorted(self.v_disc, self.v[i]),
+                        state[1],
+                        state[2],
+                    ]
                     v_new[i] = self.v[i]
 
             else:
-                action = 'slowdown'
+                action = "slowdown"
                 v_new[i] = v_des
-                
+
         # zapisanie poprzedniego stanu
-        
+
         self.history_v[step] = self.v
         self.history_x[step] = self.x % self.road_length
 
         # wprowadzenie nowego stanu
-        
+
         for i in range(n):
             x_new[i] = self.x[i] + v_new[i]
-        
+
         # aktualizacja Q-table
 
         for i in range(n):
-            if action == 'steady':
-                reward = v_new[(i-1) % n] - self.history_v[step][(i-1) % n]
-                self.q_table_steady[state] = (1 - self.alpha) * self.q_table_steady[state] + reward + self.gamma * self.q_table_steady[state_new]
-            elif action == 'accelerate':
-                reward = v_new[(i-1) % n] - self.history_v[step][(i-1) % n]
-                self.q_table_accelerate[state] = (1 - self.alpha) * self.q_table_accelerate[state] + reward + self.gamma * self.q_table_accelerate[state_new]
+            if action == "steady":
+                reward = v_new[(i - 1) % n] - self.history_v[step][(i - 1) % n]
+                self.q_table_steady[state] = (1 - self.alpha) * self.q_table_steady[state] + self.alpha * (reward + self.gamma * self.q_table_steady[state_new])
+            elif action == "accelerate":
+                reward = v_new[(i - 1) % n] - self.history_v[step][(i - 1) % n]
+                self.q_table_accelerate[state] = (1 - self.alpha) * self.q_table_accelerate[state] + self.alpha * (reward + self.gamma * self.q_table_accelerate[state_new])
 
         return np.sort(x_new), v_new
-    
+
     def _get_stats(self):
         n = np.size(self.history_x, 1)
         avg_speed = [np.mean(self.history_v[i]) for i in range(self.time)]
@@ -120,79 +184,111 @@ class QLearningSimulation:
         avg_position = [np.mean(self.history_x[i]) for i in range(self.time)]
         std_position = [np.std(self.history_x[i]) for i in range(self.time)]
 
-        gaps = [[(self.history_x[j][(i+1) % n] - self.history_x[j][i]) % self.road_length for i in range(n)] for j in range(self.time)]
+        gaps = [
+            [
+                (self.history_x[j][(i + 1) % n] - self.history_x[j][i])
+                % self.road_length
+                for i in range(n)
+            ]
+            for j in range(self.time)
+        ]
         gaps_avg = [np.mean(gaps[i]) for i in range(self.time)]
         gaps_max = [np.max(gaps[i]) for i in range(self.time)]
         gaps_min = [np.min(gaps[i]) for i in range(self.time)]
-    
-        return avg_speed, max_speed, min_speed, avg_position, std_position, gaps_avg, gaps_max, gaps_min
-    
+
+        return (
+            avg_speed,
+            max_speed,
+            min_speed,
+            avg_position,
+            std_position,
+            gaps_avg,
+            gaps_max,
+            gaps_min,
+        )
+
     def _plot_space(self):
         n = len(self.history_x[0])
         for i in range(self.time):
-            plt.scatter(self.history_x[i], [0]*n, c=self.history_v[i], cmap='viridis', vmax=40, vmin=0)
+            plt.scatter(
+                self.history_x[i],
+                [0] * n,
+                c=self.history_v[i],
+                cmap="viridis",
+                vmax=self.v_max,
+                vmin=0,
+            )
             plt.colorbar()
             plt.xlim((0, self.road_length))
-            plt.title(f't = {i} $\Delta t = 1$')
+            plt.title(f"t = {i} $\Delta t = 1$")
             plt.savefig(f'Krauss/graphs/{str(i+1).rjust(4, "0")}.png')
             plt.clf()
-    
+
     def _plot_to_gif(self, path):
-        source = f'{os.getcwd()}/Krauss/graphs'
-        images = [Image.open(source + '/' + file) for file in os.listdir(source)]
-        images[0].save(f'{path}/simulation_gif.gif', 
-                    save_all=True,
-                    append_images=images[1:],
-                    duration=100,
-                    loop=0
-                    )
-        for file in os.listdir(f'{os.getcwd()}/Krauss/graphs'):
-            os.remove(os.path.join(f'{os.getcwd()}/Krauss/graphs', file))
-    
+        source = f"{os.getcwd()}/Krauss/graphs"
+        images = [Image.open(source + "/" + file) for file in os.listdir(source)]
+        images[0].save(
+            f"{path}/simulation_gif.gif",
+            save_all=True,
+            append_images=images[1:],
+            duration=100,
+            loop=0,
+        )
+        for file in os.listdir(f"{os.getcwd()}/Krauss/graphs"):
+            os.remove(os.path.join(f"{os.getcwd()}/Krauss/graphs", file))
+
     def _plot_speed_stats(self, path, avg_speed, max_speed, min_speed):
         t = np.arange(self.time)
-        plt.plot(t, avg_speed, color='red', label='$v_{śr}$')
-        plt.plot(t, max_speed, color='green', label='$v_{max}$')
-        plt.plot(t, min_speed, color='blue', label='$v_{min}$')
+        plt.plot(t, avg_speed, color="red", label="$v_{śr}$")
+        plt.plot(t, max_speed, color="green", label="$v_{max}$")
+        plt.plot(t, min_speed, color="blue", label="$v_{min}$")
         plt.legend()
-        plt.xlabel('Czas')
-        plt.ylabel('Prędkość')
-        plt.savefig(f'{path}/speed_statistics.png')
+        plt.xlabel("Czas")
+        plt.ylabel("Prędkość")
+        plt.savefig(f"{path}/speed_statistics.png")
         plt.clf()
 
     def _plot_position_stats(self, path, avg_position, std_position):
         t = np.arange(self.time)
-        plt.plot(t, avg_position, color='red', label='$x_{śr}$')
-        plt.plot(t, std_position, color='blue', label='$\sigma_x$')
+        plt.plot(t, avg_position, color="red", label="$x_{śr}$")
+        plt.plot(t, std_position, color="blue", label="$\sigma_x$")
         plt.legend()
-        plt.xlabel('Czas')
-        plt.ylabel('Wartość x')
-        plt.savefig(f'{path}/position_statistics.png')
+        plt.xlabel("Czas")
+        plt.ylabel("Wartość x")
+        plt.savefig(f"{path}/position_statistics.png")
         plt.clf()
 
     def _plot_gaps_stats(self, path, avg_gaps, max_gaps, min_gaps):
         t = np.arange(self.time)
-        plt.plot(t, avg_gaps, color='red', label='$d_{śr}$')
-        plt.plot(t, max_gaps, color='green', label='$d_{max}$')
-        plt.plot(t, min_gaps, color='blue', label='$d_{min}$')
+        plt.plot(t, avg_gaps, color="red", label="$d_{śr}$")
+        plt.plot(t, max_gaps, color="green", label="$d_{max}$")
+        plt.plot(t, min_gaps, color="blue", label="$d_{min}$")
         plt.legend()
-        plt.xlabel('Czas')
-        plt.ylabel('Odległości między samochodami')
-        plt.savefig(f'{path}/gaps_statistics.png')
+        plt.xlabel("Czas")
+        plt.ylabel("Odległości między samochodami")
+        plt.savefig(f"{path}/gaps_statistics.png")
         plt.clf()
 
     def _plot_history(self, path):
         n = len(self.history_x[0])
         for i in range(self.time):
-            plt.scatter(self.history_x[i], [i]*n, c=self.history_v[i], cmap='viridis', vmax=40, vmin=0)
+            plt.scatter(
+                self.history_x[i],
+                [i] * n,
+                c=self.history_v[i],
+                cmap="viridis",
+                vmax=self.v_max,
+                vmin=0,
+            )
         plt.colorbar()
         plt.xlim((0, self.road_length))
         plt.gca().invert_yaxis()
-        plt.savefig(f'{path}/simulation.png')
+        plt.savefig(f"{path}/simulation.png")
         plt.clf()
 
     ###########################################################
 
-if __name__ == '__main__':
-    QLearningSimulation(4, time=300, alpha=0.8, gamma=0.8).simulation()
-
+if __name__ == "__main__":
+    
+    for i in range(5):
+        QLearningSimulation(i+1, time=5000).simulation()
