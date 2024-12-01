8dccec1ccfe114159320f51778f38f94b61dcc96
Oskar Matysik <oskar.orion18@gmail.com>
2024-12-01 14:44:24 +0100

    poprawka w parametrze epsilon

--- Krauss/krauss_model.py
+++ Krauss/krauss_model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.colors as colors
 import os
+import pandas as pd
 from PIL import Image
 
 
@@ -23,7 +24,8 @@ class KraussSimulation:
         create_gif=False,
         jam_speed_coeff=0.2,
         jam_gap_coeff=0.2,
-        jam_cars_involved_coeff=0.1
+        jam_cars_involved_coeff=0.1,
+        only_stats=False
     ):
         self.num = num
         self.acc_coeff = acc_coeff
@@ -41,25 +43,29 @@ class KraussSimulation:
         self.jam_speed_coeff = jam_speed_coeff
         self.jam_gap_coeff = jam_gap_coeff
         self.jam_cars_involved_coeff = jam_cars_involved_coeff
+        self.only_stats = only_stats
 
     def _generate_cars(self):
-        x = np.sort(np.random.uniform(0, self.road_length, self.num_cars))
-        v = np.random.uniform(0, self.v_max, self.num_cars)
+        # x = np.sort(np.random.uniform(0, self.road_length, self.num_cars))
+        # v = np.random.uniform(0, self.v_max, self.num_cars)
+        x = np.linspace(0, self.road_length, self.num_cars)
+        v = np.zeros(self.num_cars)
 
         return x, v
 
     def simulation(self):
 
-        path = f'{os.getcwd()}/Krauss/simulations/{str(self.num).rjust(2, "0")}'
-        os.mkdir(path)
         self._krauss_simulation()
-        if self.create_gif:
-            self._plot_space()
-            self._plot_to_gif(path)
-        self._plot_speed_stats(path)
-        self._plot_traffic_flow_stats(path)
-        self._plot_jam_stats(path)
-        self._plot_history(path)
+        if not self.only_stats:
+            path = f'{os.getcwd()}/Krauss/simulations/{str(self.num).rjust(2, "0")}'
+            os.mkdir(path)
+            if self.create_gif:
+                self._plot_space()
+                self._plot_to_gif(path)
+            self._plot_speed_stats(path)
+            self._plot_traffic_flow_stats(path)
+            self._plot_jam_stats(path)
+            # self._plot_history(path)
 
     def _krauss_simulation(self):
 
@@ -80,7 +86,10 @@ class KraussSimulation:
                 self.reaction_time + full_stop_time
             )
             v_des = min([self.v_max, self.v[i] + self.acc_coeff, v_safe])
-            v_new[i] = max(0, v_des - np.random.uniform(0, self.eps))
+            if v_des == self.v_max or v_des == v_safe:
+                v_new[i] = max(0, v_des - np.random.uniform(0, self.eps))
+            else:
+                v_new[i] = max(0, v_des)
 
         self.history_v[step] = self.v
         self.history_x[step] = self.x % self.road_length
@@ -132,7 +141,7 @@ class KraussSimulation:
         plt.legend()
         plt.xlabel("Czas")
         plt.ylabel("Prędkość")
-        plt.savefig(f"{path}/speed_statistics.png")
+        plt.savefig(f"{path}/velocity.png")
         plt.clf()
 
     def _plot_traffic_flow_stats(self, path):
@@ -144,8 +153,29 @@ class KraussSimulation:
         plt.legend()
         plt.xlabel("Czas")
         plt.ylabel("Przepływ")
-        plt.savefig(f"{path}/position_statistics.png")
+        plt.savefig(f"{path}/flow.png")
         plt.clf()
+    
+    def get_jam_stats(self):
+        t = np.arange(self.time)
+        g_hom = self.num_cars / self.road_length
+        v_hom = g_hom
+        gaps = np.array([
+            [
+                (self.history_x[j][(i + 1) % self.num_cars] - self.history_x[j][i])
+                % self.road_length
+                for i in range(self.num_cars)
+            ]
+            for j in range(self.time)
+        ])
+        
+        traffic_jam_state = self.jam_cars_involved_coeff * self.num_cars
+        for i in range(self.time):
+            jam_velocity_condition = self.history_v[i] < self.jam_speed_coeff * v_hom
+            jam_gap_condition = gaps[i] < self.jam_gap_coeff * g_hom
+            if np.sum(jam_gap_condition & jam_velocity_condition) > traffic_jam_state:
+                return i
+        return None
 
     def _plot_jam_stats(self, path):
         t = np.arange(self.time)
@@ -165,9 +195,7 @@ class KraussSimulation:
         for i in range(self.time):
             jam_velocity_condition = self.history_v[i] < self.jam_speed_coeff * v_hom
             jam_gap_condition = gaps[i] < self.jam_gap_coeff * g_hom
-            # print(gaps)
-            # print(jam_gap_condition)
-            jam_cars[i] = np.sum(jam_gap_condition & jam_velocity_condition)
+            jam_cars[i] = np.sum(jam_gap_condition | jam_velocity_condition)
         
         plt.figure(dpi=1200)
         plt.plot(t, jam_cars, color="blue", label="Liczba pojazdów")
@@ -194,12 +222,34 @@ class KraussSimulation:
         plt.colorbar()
         plt.xlim((0, self.road_length))
         plt.gca().invert_yaxis()
-        plt.axis('scaled')
+        # plt.axis('scaled')
         plt.savefig(f"{path}/simulation.png")
         plt.clf()
 
+def multi_simulation_krauss(number_of_simulations, eps, time):
+    avg_velocity = 0
+    avg_congestion_time = 0 
+    for i in range(number_of_simulations):
+        print(i)
+        sim = KraussSimulation(i, eps=eps, time=time)
+        avg_velocity += np.mean(sim.history_v)
+        avg_congestion_time += sim.get_jam_stats()
+    return avg_velocity/number_of_simulations, avg_congestion_time/number_of_simulations
+
+def jam_emergence_and_average_velocity(eps_list, time_list, sim_num=1000):
+    velocity_results = np.empty(len(eps_list))
+    congestion_results = np.empty(len(eps_list))
+    for i in range(len(eps_list)):
+        eps = eps_list[i]
+        time = time_list[i]
+        velocity_results[i], congestion_results[i] = multi_simulation_krauss(1000, eps, time)
+    return velocity_results, congestion_results
     ###########################################################
 
 
 if __name__ == "__main__":
-    KraussSimulation(19, eps=1, time=500).simulation()
+    # KraussSimulation(10, eps=1, time=10**5).simulation()
+    eps_list = np.linspace(0.5, 1, 51, endpoint=True)
+    time_list = np.ones(51) * 1000
+    v, c = jam_emergence_and_average_velocity(eps_list, time_list, sim_num=100)
+    print(v, c)
